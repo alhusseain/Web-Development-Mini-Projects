a38acdb50846add112bebc5707e68ea6b19ae2b2
alhusseain <husseainshalaby6@gmail.com>
2025-03-07 23:59:47 +0200

    assignment 2

--- NodeJS/expressJS/books.js
+++ NodeJS/expressJS/books.js
@@ -5,11 +5,13 @@ const router = express.Router()
 var books=[{
     'Title':'Chronicles of Narnia',
     'Author': 'Donut rememba',
-    'ISBN':'219840'
+    'ISBN':'219840',
+    'Status':'Available'
 },{
     'Title':'Sherlock Holmes vol.1',
     'Author':'Sir Arthur Conan Doyle',
-    'ISBN':'12874692'
+    'ISBN':'12874692',
+    'Status':'Available'
 }
 ]
 
@@ -24,25 +26,41 @@ router.get('/id/:id',(req,res)=>{
 router.post('/add/:title/:author/:isbn',(req,res)=>{
     books.push({'Title':req.params.title,
         'Author':req.params.author,
-        'ISBN':req.params.isbn
+        'ISBN':req.params.isbn,
+        'Status':'Available'
     })
 
     res.send("Added Successfuly!")
 
 })
 
-router.post('/remove/:id',(req,res)=>{
+router.post('/borrow/:id',(req,res)=>{
+    if(books[req.params.id].Status!='Available')
+    {
+        res.send("Book already borrowed !")
+    }
+    else
+    {
+        books[req.params.id].Status='Borrowed';
+        res.send("Book borrowed successfully!")
+    }
+})
+
+router.post('/return/:id',(req,res)=>{
+    if(books[req.params.id].Status != 'Borrowed') res.send("Book not Borrowed!")
+    else {books[req.params.id].Status='Available';res.send("Book returned Successfully")}
+})
+
+router.delete('/remove/:id',(req,res)=>{
     console.log("here");
     books.splice(req.params.id,1)
     res.send("Removed Successfuly!")
 })
 
-router.post('/update/:id/:title/:author/:isbn',(req,res)=>{
-    books[req.params.id]={
-        'Title':req.params.title,
-        'Author': req.params.author,
-        'ISBN': req.params.isbn
-    }
+router.patch('/update/:id/:title/:author/:isbn',(req,res)=>{
+    books[req.params.id].Author=req.params.author
+    books[req.params.id].Title=req.params.title
+    books[req.params.id].ISBN=req.params.isbn
     res.send("Updated Successfuly!")
 })
 
--- NodeJS/expressJS/server.js
+++ NodeJS/expressJS/server.js
@@ -3,6 +3,11 @@ const app = express()
 const books = require("./books.js")
 const port = 8080
 
+app.use((req,res,next)=>{
+    if(req.headers.authorization!="Bearer ZEWAIL") res.send("Not authorized!")
+    else next()
+})
+
 app.use("/book",books)
 
 app.get("/",(req,res)=>{res.send("welcome")})
